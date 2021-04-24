378cd24c88987e123b8050100600cb5fe72c1eab
Julio <julio.rosa@triasoftware.com.br>
2021-04-23 21:04:45 -0300

    feat customer crud

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
@@ -13,19 +17,50 @@ export class CustomerService {
   ) {}
 
   async create(createCustomerDto: CreateCustomerDto): Promise<CustomerEntity> {
+    const { email, cpf } = createCustomerDto;
+
+    const costumerWithEmail = await this.costumerRepository.findByEmail(email);
+    if (costumerWithEmail) {
+      throw new ConflictException('E-mail already in use');
+    }
+
+    const costumerWithCpf = await this.costumerRepository.findByCpf(cpf);
+    if (costumerWithCpf) {
+      throw new ConflictException('Cpf already in use');
+    }
+
     const customer = this.costumerRepository.create(createCustomerDto);
     return this.costumerRepository.save(customer);
   }
 
-  findAll() {}
+  findAll(): Promise<CustomerEntity[]> {
+    return this.costumerRepository.find();
+  }
 
-  findOne(id: number) {}
+  async findOne(id: number): Promise<CustomerEntity> {
+    const customer = await this.costumerRepository.findById(id);
+    if (!customer) {
+      throw new NotFoundException(`Customer with id: ${id} not found`);
+    }
+    return customer;
+  }
 
   async update(
     id: number,
     updateCustomerDto: UpdateCustomerDto,
   ): Promise<CustomerEntity> {
     const customer = await this.costumerRepository.findById(id);
+    const { email, cpf } = updateCustomerDto;
+
+    const costumerWithEmail = await this.costumerRepository.findByEmail(email);
+    if (costumerWithEmail && costumerWithEmail.id !== id) {
+      throw new ConflictException('E-mail already in use');
+    }
+
+    const costumerWithCpf = await this.costumerRepository.findByCpf(cpf);
+    if (costumerWithCpf && costumerWithCpf.id !== id) {
+      throw new ConflictException('Cpf already in use');
+    }
 
     return this.costumerRepository.save({
       ...customer,
@@ -33,5 +68,11 @@ export class CustomerService {
     });
   }
 
-  delete(id: number) {}
+  async delete(id: number) {
+    const result = await this.costumerRepository.delete(id);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Customer with id: ${id} not found`);
+    }
+  }
 }
--- src/customer/repository/customer.repository.ts
+++ src/customer/repository/customer.repository.ts
@@ -3,7 +3,15 @@ import { CustomerEntity } from '../entities/customer.entity';
 
 @EntityRepository(CustomerEntity)
 export class CustomerRepository extends Repository<CustomerEntity> {
-  findById(id: number) {
+  findById(id: number): Promise<CustomerEntity> {
     return this.findOne({ id });
+  }
+
+  findByEmail(email: string): Promise<CustomerEntity> {
+    return this.findOne({ email });
+  }
+
+  findByCpf(cpf: string): Promise<CustomerEntity> {
+    return this.findOne({ cpf });
   }
 }
