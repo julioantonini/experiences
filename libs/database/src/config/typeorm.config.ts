1cdd180449a00eae5681e98df0d3e8f7a3c423d3
julio <julio.antonini@gylabs.com.br>
2024-05-11 20:45:20 -0300

    feat: adjust to upgrade typeorm

--- libs/database/src/config/typeorm.config.ts
+++ libs/database/src/config/typeorm.config.ts
@@ -10,7 +10,7 @@ module.exports = {
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
-  database: 'postgres',
+  database: process.env.DB_NAME || 'postgres',
   synchronize: false,
   migrationsRun: true,
   logging: process.env.NODE_ENV === 'local',
